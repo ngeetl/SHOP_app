28ec29a8a53a54501ede0acdbebf4891b9edddc0
yeni <ngeetl@naver.com>
2023-11-02 23:24:35 +0900

    tailwindCSS를 이용해 전체적인 구조 설정 및 react-icons 설치 및 사용

--- frontend/src/layout/Footer/index.jsx
+++ frontend/src/layout/Footer/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { FcApproval } from "react-icons/fc";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className='flex items-center'>
+      <FcApproval />
+      <p className='ml-2'>All rights reserved.</p>
     </div>
   )
 }
--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const LandingPage = () => {
   return (
-    <div>
+    <div className='font-bold'>
       LandingPage
     </div>
   )
