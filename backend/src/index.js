0000decab2d0900922cabef0cb558ddd8345d08b
yeni <ngeetl@naver.com>
2023-11-08 20:56:52 +0900

    cors, express.json()로 미들웨어 초기화

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
-
+const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 4000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 })
 
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+})
+
+app.use(express.static(path.join(__dirname, 'uploads')))
 
 app.listen(port, () => {
     console.log('server is running!');
