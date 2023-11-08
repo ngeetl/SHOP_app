8c475f142760bf6517e64700e51f5155760ca6f3
yeni <ngeetl@naver.com>
2023-11-08 19:44:14 +0900

    redux-persist 설치 및 초기화

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from './userSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer, PAUSE, PERSIST, FLUSH, REHYDRATE } from 'redux-persist'
+
+
+const rootReducer = combineReducers({
+  user: userReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  }  
+  reducer: persistedReducer,
+  middleware : getDefaultMiddleware => getDefaultMiddleware(
+    {
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST]
+      }
+    }
+  )
 })
+
+export const persistor = persistStore(store)
+
