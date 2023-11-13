78a0fbec73c29101a9a2400f29e0ea1e758a791d
yeni <ngeetl@naver.com>
2023-11-13 16:45:08 +0900

    createAsyncThunk를 이용해 비동기 액션인 registerUser를 초기화 하여 회원가입 양식 onSumit 이벤트가 일어날 때 body를 담아 dispatch하였고, extraReducers를 이용해 registerUser의 각 상태에 따른 로직 작성

--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useForm } from "react-hook-form"
-
+import { useDispatch } from 'react-redux'
+import { registerUser } from '../../store/thunkFunctions'
 
 const RegisterPage = () => {
   const {
@@ -8,10 +9,19 @@ const RegisterPage = () => {
     handleSubmit,
     formState: { errors },
     reset
-  } = useForm({ mode: 'onChange'})
+  } = useForm({ mode: 'onChange'});
+  const dispatch = useDispatch();
 
   const onSubmit = ({ email, password, name }) => {
-    
+    const body = {
+      email,
+      password,
+      name,
+      image: `https://via.placeholder.com/600x400?text=no+user+image`
+    }
+
+    dispatch(registerUser(body));
+
     reset();
   }
 
---
+++ frontend/src/store/thunkFunctions.js
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const registerUser = createAsyncThunk(
+    'user/registerUser', //액션 고유 실별자
+    async (body, thunkAPI) => {
+        try {
+            const response = await axiosInstance.post(
+                `/user/register`,
+                body
+            )
+
+            return response.data; //페이로드
+        } catch (error) {
+            console.log(error);
+            return thunkAPI.rejectWithValue(error.response.data || error.message);
+        }
+    }
+)
+
