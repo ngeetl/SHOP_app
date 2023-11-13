defc48d5a5dbbadc67e052a473e484bef073dfed
yeni <ngeetl@naver.com>
2023-11-13 23:10:10 +0900

    react-toastify 라이브러리를 이용하여 회원가입시 toast 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 import LandingPage from './pages/LandingPage/LandingPage'
 import LoginPage from './pages/LoginPage/LoginPage'
 import RegisterPage from './pages/RegisterPage/RegisterPage'
 import Navbar from './layout/Navbar'
 import Footer from './layout/Footer'
 
+
 function Layout() {
   return (
     <div className='flex flex-col h-screen justify-between'>
+
+      <ToastContainer 
+        position='bottom-right'
+        theme='light'
+        pauseOnHover
+        autoClose={1500}
+      />
+
       <Navbar />
       <main className='mb-auto w-10/12 max-w-4xl mx-auto'>
         <Outlet />
