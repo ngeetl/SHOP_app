1d13134d4a448454aa1b8ff9c025113ed570e49e
yeni <ngeetl@naver.com>
2023-11-15 23:50:28 +0900

    login route를 생성해 email을 체크하는 로직 생성 및 요청 받은 password를 비교하는 userSchema methods 생성

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -34,6 +34,14 @@ userSchema.pre('save', async function(next) {
     next();
 })
 
+userSchema.methods.comparePassword = async function(plainPassword) {
+    let user = this; // 요청한 유저의 데이터베이스 정보
+    console.log(user);
+    const match = await bcrypt.compare(plainPassword, user.password);
+    
+    return match; // true 또는 false
+}
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -13,6 +13,28 @@ router.post('/register', async (req, res, next) => {
     }
 });
 
+router.post('/login', async (req, res, next) => {
+    try {
+        // 존재하는 유저인지 체크
+        const user = await User.findOne({ email: req.body.email }); // email이 존재하면 찾은 사용자 document의 인스턴스를 반환함
+
+        if(!user) {
+            return res.status(400).send("존재하지 않는 이메일입니다.")
+        }
+
+        // 비밀번호가 올바른지 체크
+        const isMatch = await user.comparePassword(req.body.password);
+
+        if(!isMatch) {
+            return res.status(400).send('잘못된 비밀번호입니다.')
+        }
+
+    } catch (err) {
+        next(err)
+    }
+});
+
+
 
 module.exports = router;
 
