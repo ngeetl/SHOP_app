f5fec4a0d4d95fd4f7c0104ac27f1b29f8353c8f
yeni <ngeetl@naver.com>
2023-11-15 17:26:04 +0900

    bcryptjs를 이용해 userSchema에  password 암호화 작업 추가

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -19,6 +20,18 @@ const userSchema = mongoose.Schema({
         default: 0
     },
     image: String //중괄호 없이 바로 타입 입력 가능
+})
+
+userSchema.pre('save', async function(next) {
+    let user = this;
+
+    if(user.isModified('password')) {
+        const salt = await bcrypt.genSalt(10); // 괄호 안의 숫자는 복잡도
+        const hash = await bcrypt.hash(user.password, salt);
+        user.password = hash
+    }
+
+    next();
 })
 
 const User = mongoose.model("User", userSchema);
