ade6b7ac1ab0e31ffbe407232f384aec06a0169b
yeni <ngeetl@naver.com>
2023-11-18 16:24:38 +0900

    navbar UI 구조 설정

---
+++ frontend/src/layout/Navbar/Sections/NavItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavItem = () => {
+  return (
+    <div>
+      NavItem
+    </div>
+  )
+}
+
+export default NavItem
--- frontend/src/layout/Navbar/index.jsx
+++ frontend/src/layout/Navbar/index.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import NavItem from './Sections/NavItem';
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleMeun = () => {
+    setMenu(prev => !prev);
+  } 
+
   return (
-    <div>
-      Navbar
-    </div>
+    <section className='relative z-10 bg-orange-500 text-white'>
+      <div className='w-full'>
+        
+        <div className='flex items-center justify-between mx-5 sm:mx-10 lg:mx-20'>
+          {/* logo */}
+          <div className='flex items-center text-3xl h-16 font-semibold'>
+            <Link to="/">Logo</Link>
+          </div>
+          {/* menu button */}
+          <div className='text-2xl sm:hidden'>
+            <button onClick={handleMeun}>{menu ? "-" : "+"}</button>
+          </div>
+          {/* big screen nav-items */}
+          <div className='hidden sm:block'>
+            <NavItem />
+          </div>
+        </div>
+
+
+        {/* mobile nav-items */}
+        <div className='block sm:hidden'>
+          {menu && <NavItem />}
+        </div>
+
+      </div>
+    </section>
   )
 }
 
